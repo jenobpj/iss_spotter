1b412df4a78525735fe58ba3d7646f55cb231b6a
jenobpj <jenobgit@gmail.com>
2021-09-23 22:00:17 -0400

    spotter added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchMyIp} =require('./iss')
+const {fetchMyIp, fetchCoordsByIp} =require('./iss')
 
 fetchMyIp((error,ip)=>{
   if(error){
@@ -6,4 +6,12 @@ fetchMyIp((error,ip)=>{
     return
   }
   console.log('it worked Returned IP',ip)
+})
+fetchCoordsByIp('99.227.137.11',(error,data)=>{
+  if(error){
+    console.log('it did not work',error)
+    return;
+  }
+  console.log('it worked',data)
+
 })
--- iss.js
+++ iss.js
@@ -12,4 +12,18 @@ const ip=JSON.parse(body).ip;
 callback(null,ip)
 })
 }
-module.exports={fetchMyIp}
+const fetchCoordsByIp=(ip,callback)=>{
+  request(`https://freegeoip.app/json/${ip}`,(error,response,body)=>{
+  if(error) return callback(error,null)
+  if(response.statusCode !==200){
+    const msg=`status Code${response.statusCode} when fetching ip.Response:${body}`;
+    callback(Error(msg),null);
+    return;
+  }
+  
+  const { latitude, longitude } = JSON.parse(body);
+      callback(null, { latitude, longitude });
+})
+  
+}
+module.exports={fetchMyIp,fetchCoordsByIp}
